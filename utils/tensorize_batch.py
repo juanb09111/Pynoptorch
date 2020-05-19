ed03994aeac6b467fee7e99e9f9b82bac2b6d6e7
juanb09111 <juanpablo.lagosbenitez@tuni.fi>
2020-05-20 02:25:03 +0300

    PYN-7 #comment Create function to tensorize input batch

---
+++ utils/tensorize_batch.py
@@ -0,0 +1,12 @@
+import torch
+
+def tensorize_batch(batch):
+    batch_size= len(batch)
+    sample = batch[0]
+
+    res = torch.zeros(batch_size, *sample.shape)
+
+    for i in range(batch_size):
+        res[i] = torch.tensor(batch[i])
+
+    return res
