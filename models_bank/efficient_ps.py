90e397ffd0238fe375a700155de78cfe1921f62b
juanb09111 <juanpablo.lagosbenitez@tuni.fi>
2020-06-06 10:42:02 +0300

    PYN-11 #comment Use effB3, apply transform to output

--- models_bank/efficient_ps.py
+++ models_bank/efficient_ps.py
@@ -3,6 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 import torchvision
 from collections import OrderedDict
+from torchvision.models.detection.transform import GeneralizedRCNNTransform
 from common_blocks.image_list import ImageList
 from backbones_bank.efficient_ps_backbone import efficient_ps_backbone as eff_net
 from segmentation_heads.RPN import RPN
@@ -13,10 +14,12 @@ import config
 
 
 class EfficientPS(nn.Module):
-    def __init__(self, backbone_out_channels, num_ins_classes, num_sem_classes):
-        super().__init__()
-
-        self.backbone = eff_net(1.6, 2.2, (512, 1024))
+    def __init__(self, backbone_out_channels, num_ins_classes, num_sem_classes, min_size=800, max_size=1333, image_mean=None, image_std=None):
+        super(EfficientPS, self).__init__()
+        
+        self.backbone = eff_net(1.2, 1.4, (512, 1024))
+        # self.backbone = eff_net(1.6, 2.2, (512, 1024))
+        # self.backbone = eff_net(1, 1, (512, 1024))
 
         self.semantic_head = sem_seg_head(
             backbone_out_channels,
@@ -25,11 +28,23 @@ class EfficientPS(nn.Module):
         self.rpn = RPN(256)
         self.roi_pool = roi_heads(num_ins_classes + 1)
 
+        if image_mean is None:
+            image_mean = [0.485, 0.456, 0.406]
+        if image_std is None:
+            image_std = [0.229, 0.224, 0.225]
+
+        self.transform = GeneralizedRCNNTransform(min_size, max_size, image_mean, image_std)
+
+
         for module in self.children():
             if self.training:
                 module.training = True
             else:
                 module.training = False
+
+    def to(self, device):
+        for module in self.children():
+            module.to(device)
 
     def forward(self, images, anns=None):
 
@@ -42,32 +57,36 @@ class EfficientPS(nn.Module):
                                     ('P8', P8),
                                     ('P16', P16),
                                     ('P32', P32)])
-                                    
+
         image_list = ImageList(images, [x.shape[1:] for x in images])
         image_sizes = [x.shape[1:] for x in images]
-
+        
         proposals, proposal_losses = self.rpn(
             image_list, feature_maps, anns)
-        
-        roi_result, roi_losses = self.roi_pool(feature_maps, proposals, image_sizes, targets=anns)
 
+        roi_result, roi_losses = self.roi_pool(
+            feature_maps, proposals, image_sizes, targets=anns)
+
+        roi_result = self.transform.postprocess(roi_result, image_sizes, image_sizes)
         if self.training:
             semantic_masks = list(
                 map(lambda ann: ann['semantic_mask'], anns))
-            semantic_masks = tensorize_batch(semantic_masks)
+            semantic_masks = tensorize_batch(semantic_masks, config.DEVICE)
 
             losses["semantic_loss"] = F.cross_entropy(
                 semantic_logits, semantic_masks.long())
 
-            losses["rpn"] = proposal_losses
-
-            losses['roi'] = roi_losses
+            losses = {**losses, **proposal_losses, **roi_losses}
 
             return losses
 
         else:
-            return semantic_logits, roi_result
+            # return {**roi_result, 'semantic_logits': semantic_logits}
+            return [{**res, 'semantic_logits': semantic_logits[idx]} for idx, res in enumerate(roi_result)]
 
+
+# device = torch.device(
+#         'cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 # data_loader_val = torch.load(config.DATA_LOADER_VAL_FILENAME)
 
@@ -75,14 +94,17 @@ class EfficientPS(nn.Module):
 
 # images, anns = next(iterator)
 
-# images = tensorize_batch(images)
+# images = tensorize_batch(images, device)
 
 # model = EfficientPS(256, 7, 1)
-# model.train()
-# losses = model(images, anns=anns)
-# print(losses)
+
+# model.to(device)
+# # model.train()
+# # losses = model(images, anns=anns)
+# # print(losses)
 
 # model.eval()
-# semantic_logits, roi_result = model(images)
+# res = model(images)
+# print(len(res), res[0].keys())
 
 
