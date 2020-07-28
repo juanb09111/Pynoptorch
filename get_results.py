c180f0505a8a87738c695f01a40302684d60b08d
juanb09111 <juanpablo.lagosbenitez@tuni.fi>
2020-07-28 19:19:11 +0300

    PYN-19 #comment Purge get_results module

--- get_results.py
+++ get_results.py
@@ -1,13 +1,3 @@
-import torchvision
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-from pycocotools.coco import COCO
-import numpy as np
-import skimage.io as io
-import matplotlib.pyplot as plt
-from collections import OrderedDict
-from PIL import Image
-
 import config
 import constants
 import models
@@ -16,25 +6,19 @@ from utils.tensorize_batch import tensorize_batch
 
 import os
 import os.path
-import numpy as np
 import torch
-import json
 
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-import matplotlib.patches as patches
-import cv2
-import random
-from utils import show_bbox, fasterrcnn_resnet50_fpn, get_datasets, iss_model
+from utils import show_bbox, panoptic_fusion
 import glob
-import re
 
 
 device = torch.device(
     'cuda') if torch.cuda.is_available() else torch.device('cpu')
 print(device)
+
+config.DEVICE = device
 torch.cuda.empty_cache()
-# move model to the right device
 
 
 def view_masks(model,
@@ -45,8 +29,10 @@ def view_masks(model,
                folder,
                confidence=0.5):
     model.load_state_dict(torch.load(weights_file))
+    # move model to the right device
     model.to(device)
     i = 0
+    j = 0
     for images, _ in data_loader_val:
         images = list(img for img in images)
         images = tensorize_batch(images, device)
@@ -54,6 +40,11 @@ def view_masks(model,
         model.eval()
         with torch.no_grad():
             outputs = model(images)
+
+            if result_type == "panoptic":
+                panoptic_fusion.get_panoptic_results(images, outputs, folder, j)
+                j += 1
+                torch.cuda.empty_cache()
 
             for idx, output in enumerate(outputs):
                 i += 1
@@ -67,8 +58,9 @@ def view_masks(model,
 
                 torch.cuda.empty_cache()
 
-
 if __name__ == "__main__":
+    torch.cuda.empty_cache()
+
     data_loader_val_filename = os.path.join(os.path.dirname(
         os.path.abspath(__file__)), constants.DATA_LOADERS_LOC, constants.DATA_LOADER_VAL_FILENAME)
     data_loader_val = torch.load(data_loader_val_filename)
@@ -85,4 +77,10 @@ if __name__ == "__main__":
                config.MODEL_WEIGHTS_FILENAME,
                "semantic",
                '{}_results_semantic_resnet'.format(config.MODEL),
+               confidence=0.5)
+    
+    view_masks(model, data_loader_val, config.NUM_THING_CLASSES + config.NUM_THING_CLASSES,
+               config.MODEL_WEIGHTS_FILENAME,
+               "panoptic",
+               '{}_results_panoptic_resnet'.format(config.MODEL),
                confidence=0.5)
