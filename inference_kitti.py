6f0e53e9d799e9a3e1b2a0743986366aa9b70af3
Juan <juanpablo.lagosbenitez@tuni.fi>
2021-04-13 15:08:16 +0300

    PYN-30 #comment Inference on Kitti

--- inference_kitti.py
+++ inference_kitti.py
@@ -10,7 +10,7 @@ import os.path
 from pathlib import Path
 import torch
 from torch import nn
-from utils.get_kitti_dataset import get_dataloaders
+from utils.get_kitti_testset import get_dataloaders
 
 from datetime import datetime
 
@@ -24,10 +24,13 @@ print(device)
 temp_variables.DEVICE = device
 torch.cuda.empty_cache()
 
-def RMSE(sparse_depth_gt,pred):
-    mask_gt = torch.where(sparse_depth_gt > 0, torch.tensor((1), device=temp_variables.DEVICE, dtype=torch.float64), torch.tensor((0), device=temp_variables.DEVICE, dtype=torch.float64))
+
+def RMSE(sparse_depth_gt, pred):
+    mask_gt = torch.where(sparse_depth_gt > 0, torch.tensor((1), device=temp_variables.DEVICE,
+                                                            dtype=torch.float64), torch.tensor((0), device=temp_variables.DEVICE, dtype=torch.float64))
     mask_gt = mask_gt.squeeze_(1)
-    sparse_depth_gt = sparse_depth_gt.squeeze_(1) # remove C dimension there's only one
+    sparse_depth_gt = sparse_depth_gt.squeeze_(
+        1)  # remove C dimension there's only one
     c = torch.tensor((1000), device=device)
     sparse_depth_gt = sparse_depth_gt*c
     pred = pred*c
@@ -52,16 +55,15 @@ def predict(model,
     model.eval()
 
     with torch.no_grad():
-        
-        for i, (imgs, lidar_fov, masks, sparse_depth, k_nn_indices, sparse_depth_gt, img_names) in enumerate(data_loader_val):
+
+        for i, (imgs, lidar_fov, masks, sparse_depth, k_nn_indices, img_names) in enumerate(data_loader_val):
 
             imgs = list(img for img in imgs)
             lidar_fov = list(lid_fov for lid_fov in lidar_fov)
             masks = list(mask for mask in masks)
             sparse_depth = list(sd for sd in sparse_depth)
             k_nn_indices = list(k_nn for k_nn in k_nn_indices)
-            sparse_depth_gt = list(sp_d for sp_d in sparse_depth_gt)
-            img_names = list(image_name for image_name  in img_names)
+            img_names = list(image_name for image_name in img_names)
 
             imgs = tensorize_batch(imgs, device)
             lidar_fov = tensorize_batch(lidar_fov, device, dtype=torch.float)
@@ -69,16 +71,14 @@ def predict(model,
             sparse_depth = tensorize_batch(sparse_depth, device)
             k_nn_indices = tensorize_batch(
                 k_nn_indices, device, dtype=torch.long)
-            sparse_depth_gt = tensorize_batch(
-                sparse_depth_gt, device, dtype=torch.float)
 
             _, outputs = model(imgs, sparse_depth, masks,
-                               lidar_fov, k_nn_indices, sparse_depth_gt)
+                               lidar_fov, k_nn_indices, sparse_depth_gt=None)
 
             for idx in range(outputs.shape[0]):
 
                 # rmse = RMSE(sparse_depth_gt, outputs[idx])
-                
+
                 out = outputs[idx].cpu().numpy()
                 img = imgs[idx]
 
@@ -104,30 +104,29 @@ def predict(model,
 
                 f.savefig('{}/{}.png'.format(folder, img_names[0]))
                 plt.close(f)
-                
 
 
 if __name__ == "__main__":
 
-    imgs_root_val = os.path.join(os.path.dirname(os.path.abspath(__file__)), config_kitti.TEST_DIR, "imgs/2011_09_26/val/")
+    imgs_root_val = os.path.join(os.path.dirname(os.path.abspath(
+        __file__)), config_kitti.TEST_DIR, "imgs/2011_09_26/val/")
 
-    data_depth_velodyne_root_val = os.path.join(os.path.dirname(os.path.abspath(__file__)), config_kitti.TEST_DIR, "data_depth_velodyne/val/")
-    
-    data_depth_annotated_root_val = os.path.join(os.path.dirname(os.path.abspath(__file__)), config_kitti.TEST_DIR, "data_depth_annotated/val/")
+    data_depth_velodyne_root_val = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), config_kitti.TEST_DIR, "data_depth_velodyne/val/")
+
     torch.cuda.empty_cache()
 
     calib_velo2cam = calib_filename = os.path.join(os.path.dirname(os.path.abspath(
-            __file__)), config_kitti.DATA, "imgs/2011_09_26/calib_velo_to_cam.txt")
+        __file__)), config_kitti.DATA, "imgs/2011_09_26/calib_velo_to_cam.txt")
     calib_cam2cam = calib_filename = os.path.join(os.path.dirname(os.path.abspath(
         __file__)), config_kitti.DATA, "imgs/2011_09_26/calib_cam_to_cam.txt")
-    
-    data_loader_val = get_dataloaders(batch_size=config_kitti.BATCH_SIZE,
-                                          imgs_root=imgs_root_val,
-                                          data_depth_velodyne_root=data_depth_velodyne_root_val,
-                                          data_depth_annotated_root=data_depth_annotated_root_val,
-                                          calib_velo2cam=calib_velo2cam,
-                                          calib_cam2cam=calib_cam2cam)
 
+    data_loader_val = get_dataloaders(batch_size=config_kitti.BATCH_SIZE,
+                                      imgs_root=imgs_root_val,
+                                      data_depth_velodyne_root=data_depth_velodyne_root_val,
+                                      calib_velo2cam=calib_velo2cam,
+                                      calib_cam2cam=calib_cam2cam,
+                                      n_samples=config_kitti.MAX_TRAINING_SAMPLES)
 
     # data_loader_val = torch.load(config_kitti.DATA_LOADER_TRAIN_FILANME)
 
