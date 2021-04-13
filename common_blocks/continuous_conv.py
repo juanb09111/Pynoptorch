19606411119d5e354d2fbd5097fcb0a167eb454e
Juan <juanpablo.lagosbenitez@tuni.fi>
2021-04-13 15:06:58 +0300

    PYN-30 #comment Continous conv layer

--- common_blocks/continuous_conv.py
+++ common_blocks/continuous_conv.py
@@ -15,18 +15,35 @@ class ContinuousConvolution(nn.Module):
     + y: points features
     """
 
-    def __init__(self, n_feat, k_number, n_number):
+    def __init__(self, n_feat, k_number, n_number=None):
         super().__init__()
 
-        self.mlp = nn.Sequential(
-            # input: B x N x 3 x K
-            nn.Linear(3 * k_number, (n_feat // 2) * k_number),  # B x N x 3*(n_feat//2)
-            nn.BatchNorm1d(n_number, track_running_stats=False),  # B x N(normalize this dim) x 3*(n_feat//2)
-            nn.ReLU(),
-            nn.Linear((n_feat // 2) * k_number, n_feat * k_number),  # B x N x n_feat*k_number
-            nn.BatchNorm1d(n_number, track_running_stats=False),  # B x N(again, norm this dim) x n_feat*k_number
-            nn.ReLU()
-        )
+        self.linear1=nn.Linear(3 * k_number, (n_feat // 2) * k_number)  # B x N x 3*(n_feat//2)
+        self.batch_norm1=nn.BatchNorm1d(n_number, track_running_stats=False)  # B x N(normalize this dim) x 3*(n_feat//2)
+        self.relu1=nn.ReLU()
+
+        self.linear2=nn.Linear((n_feat // 2) * k_number, n_feat * k_number)  # B x N x n_feat*k_number
+        self.batch_norm2=nn.BatchNorm1d(n_number, track_running_stats=False)  # B x N(again, norm this dim) x n_feat*k_number
+        self.relu2=nn.ReLU()
+
+
+        # self.mlp = nn.Sequential(
+        #     # input: B x N x 3 x K
+        #     nn.Linear(3 * k_number, (n_feat // 2) * k_number),  # B x N x 3*(n_feat//2)
+        #     nn.BatchNorm1d(n_number, track_running_stats=False),  # B x N(normalize this dim) x 3*(n_feat//2)
+        #     nn.ReLU(),
+        #     nn.Linear((n_feat // 2) * k_number, n_feat * k_number),  # B x N x n_feat*k_number
+        #     nn.BatchNorm1d(n_number, track_running_stats=False),  # B x N(again, norm this dim) x n_feat*k_number
+        #     nn.ReLU()
+        # )
+
+        # self.mlp_eval = nn.Sequential(
+        #     # input: B x N x 3 x K
+        #     nn.Linear(3 * k_number, (n_feat // 2) * k_number),  # B x N x 3*(n_feat//2)
+        #     nn.ReLU(),
+        #     nn.Linear((n_feat // 2) * k_number, n_feat * k_number),  # B x N x n_feat*k_number
+        #     nn.ReLU()
+        # )
 
     def forward(self, *inputs):
         """
@@ -40,15 +57,32 @@ class ContinuousConvolution(nn.Module):
         B, N, C = x.size()
         K = indices.size(2)
 
-
-
         y1 = torch.cat([points[i, indices[i]] for i in range(B)], dim=0)
         y1 = y1.view(B,N,K,3)  # B x N x K x 3
         y1 = points[:, :, None, :] - y1  # B x N x K x 3
         y1 = y1.view(B, N, K * 3)   # B x N x K*3 
+        
+        if self.training:
+            # y1 = self.mlp(y1).view(B, N, K, C)  # reshape after mlp B x N x K x C
 
-        # output mlp: B x N x K*C
-        y1 = self.mlp(y1).view(B, N, K, C)  # reshape after mlp B x N x K x C
+            # ----
+            y1 = self.linear1(y1)
+            y1 = self.batch_norm1(y1)
+            y1 = self.relu1(y1)
+
+            y1 = self.linear2(y1)
+            y1 = self.batch_norm2(y1)
+            y1 = self.relu2(y1).view(B, N, K, C)  # reshape after mlp B x N x K x C
+
+        if not self.training:
+            print("eval")
+            # y1 = self.mlp_eval(y1).view(B, N, K, C)  # reshape after mlp B x N x K x C 
+
+            y1 = self.linear1(y1)
+            y1 = self.relu1(y1)
+
+            y1 = self.linear2(y1)
+            y1 = self.relu2(y1).view(B, N, K, C)  # reshape after mlp B x N x K x C
 
         y2 = torch.cat([x[i, indices[i]] for i in range(B)], dim=0).view(B, N, K, C)  # B x N x K x C
         # print("y2 ,,", y2.shape)
